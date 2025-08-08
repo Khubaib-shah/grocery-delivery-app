d7781bd14f9187e59a448d13c1c30fcd1a60b16b
khubaib shah <bewithbaacha@gmail.com>
2025-08-08 17:41:05 +0500

    feat: add category and product models with validation

---
+++ server/src/models/categoryModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const categorySchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Category name is required"],
+    unique: true,
+    trim: true,
+    minlength: [3, "Category name must be at least 3 characters long"],
+    maxlength: [50, "Category name cannot exceed 50 characters"],
+  },
+  imageUrl: {
+    type: String,
+    required: [true, "Image URL is required"],
+  },
+});
+const Category = mongoose.model("Category", categorySchema);
+export default Category;
---
+++ server/src/models/productModel.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Product name is required"],
+  },
+  imageUrl: {
+    type: String,
+    required: [true, "Image URL is required"],
+    match: [
+      /^https?:\/\/.+\.(jpg|jpeg|png|gif)$/,
+      "Please enter a valid image URL",
+    ],
+  },
+  price: {
+    type: Number,
+    required: [true, "Product price is required"],
+    min: [0, "Price cannot be negative"],
+  },
+  discountPrice: {
+    type: Number,
+    min: [0, "Price cannot be negative"],
+  },
+  quantity: {
+    type: Number,
+    required: [true, "Product quantity is required"],
+    min: [0, "Quantity cannot be negative"],
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: [true, "Product category is required"],
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+export default Product;
