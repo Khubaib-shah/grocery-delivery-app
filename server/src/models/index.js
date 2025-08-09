857a4fd2c19f7ad6cd284d7eede8e9fe43524235
khubaib shah <bewithbaacha@gmail.com>
2025-08-09 11:34:25 +0500

    Create index file to export models for easier access

---
+++ server/src/models/index.js
@@ -0,0 +1,17 @@
+import { Customer, DeliveryPartner, Admin } from "./userModel";
+import Branch from "./branchModel";
+import Product from "./productModel";
+import Order from "./orderModel";
+import Counter from "./counterModel";
+import Category from "./categoryModel";
+
+export {
+  Order,
+  Counter,
+  Branch,
+  Customer,
+  DeliveryPartner,
+  Product,
+  Admin,
+  Category,
+};
